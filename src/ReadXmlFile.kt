0b2531e69c9f84f26274d64e21606d0b08b68703
Yahya <yahyatinani@gmail.com>
2020-08-18 23:47:10 +0100

    Make the ReadXmlFile program even more functional

--- src/ReadXmlFile.kt
+++ src/ReadXmlFile.kt
@@ -24,45 +24,63 @@ fun readDocument(xml: String, rootElementName: String): Result<List<Element>> =
             Result(List(*root.getChildren(rootElementName).toTypedArray()))
         } catch (e: IOException) {
             Result.failure("Invalid root element name `$rootElementName`" +
-                    " or XML data $xml: ${e.message}")
+                                   " or XML data $xml: ${e.message}")
         } catch (e: JDOMException) {
             Result.failure("Invalid root element name `$rootElementName`" +
-                    " or XML data $xml: ${e.message}")
+                                   " or XML data $xml: ${e.message}")
         } catch (e: Exception) {
             Result.failure("Unexpected error while reading xml data " +
-                    "$xml: ${e.message}")
+                                   "$xml: ${e.message}")
         }
     }
 
-const val format =
-    """
+val format: Pair<String, List<String>> = Pair("""
     First Name : %s
     Last Name : %s
     Email : %s
     Salary : %s
-    """
+    """, List("firstName", "lastName", "email", "salary"))
 
-fun processElement(element: Element, format: String): String =
-    String.format(format, element.getChildText("firstName"),
-        element.getChildText("lastName"),
-        element.getChildText("email"),
-        element.getChildText("salary"))
+// TODO: 2. Fix the null result when using a wrong element name
+// TODO: 3. Fix the exception thrown when you forget one of the element names
+//  in the list
+fun processElement(element: Element,
+                   format: Pair<String, List<String>>): String =
+    format.let { (format, listChildren): Pair<String, List<String>> ->
+        val parameters = listChildren.map { element.getChildText(it) }
+        String.format(format, *parameters.toArrayList().toArray())
+    }
 
-fun toStringList(elements: List<Element>, format: String): List<String> =
+fun toStringList(elements: List<Element>,
+                 format: Pair<String, List<String>>): List<String> =
     elements.map { processElement(it, format) }
 
 fun <A> process(list: List<A>): Unit = list.forEach(::println)
 
+// TODO: 1. Fix this function, it takes two args of the same type.
+fun readXmlFile(path: () -> Result<String>,
+                rootName: () -> Result<String>,
+                format: Pair<String, List<String>>,
+                effect: (List<String>) -> Unit): () -> Unit = {
+    path()
+        .flatMap { _path: String ->
+            rootName()
+                .flatMap { _rootName: String ->
+                    readFileToString(_path)
+                        .flatMap { xmlDoc: String ->
+                            readDocument(xmlDoc, _rootName)
+                                .map { list: List<Element> ->
+                                    toStringList(list, format)
+                                }
+                        }
+                }
+        }.forEach(onSuccess = effect, onFailure = { it.printStackTrace() })
+}
+
 fun main() {
-    getXmlFilePath().flatMap { path: String ->
-        getRootElementName().flatMap { rootElementName ->
-            readFileToString(path).flatMap { xmlFile: String ->
-                readDocument(xmlFile, rootElementName)
-                    .map { list: List<Element> ->
-                        toStringList(list, format)
-                    }
-            }
-        }
-    }.forEach(onSuccess = { process(it) },
-        onFailure = { it.printStackTrace() })
+    val program = readXmlFile(::getXmlFilePath,
+                              ::getRootElementName,
+                              format, ::process)
+
+    program()
 }
