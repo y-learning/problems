5eeab7d2f09c35618ed1c3471f6760c52d151803
Yahya <yahyatinani@gmail.com>
2020-08-19 11:58:29 +0100

    Extract the presentation behavior of an element

--- src/ReadXmlFile.kt
+++ src/ReadXmlFile.kt
@@ -56,33 +56,12 @@ fun readDocument(xml: String, rootElementName: String): Result<List<Element>> =
         }
     }
 
-val format: Pair<String, List<String>> = Pair("""
-    First Name : %s
-    Last Name : %s
-    Email : %s
-    Salary : %s
-    """, List("firstName", "lastName", "email", "salary"))
+fun <A> printList(list: List<A>): Unit = list.forEach(::println)
 
-// TODO: 2. Fix the null result when using a wrong element name
-// TODO: 3. Fix the exception thrown when you forget one of the element names
-//  in the list
-fun processElement(element: Element,
-                   format: Pair<String, List<String>>): String =
-    format.let { (format, listChildren): Pair<String, List<String>> ->
-        val parameters = listChildren.map { element.getChildText(it) }
-        String.format(format, *parameters.toArrayList().toArray())
-    }
-
-fun toStringList(elements: List<Element>,
-                 format: Pair<String, List<String>>): List<String> =
-    elements.map { processElement(it, format) }
-
-fun <A> process(list: List<A>): Unit = list.forEach(::println)
-
-fun readXmlFile(path: () -> FilePath,
-                rootName: () -> ElementName,
-                format: Pair<String, List<String>>,
-                effect: (List<String>) -> Unit): () -> Unit = {
+fun <T> readXmlFile(path: () -> FilePath,
+                    rootName: () -> ElementName,
+                    presenter: (Element) -> T,
+                    effect: (List<T>) -> Unit): () -> Unit = {
     path().value
         .flatMap { _path: String ->
             rootName().value
@@ -90,13 +69,26 @@ fun readXmlFile(path: () -> FilePath,
                     readFileToString(_path)
                         .flatMap { xmlDoc: String ->
                             readDocument(xmlDoc, _rootName)
-                                .map { list: List<Element> ->
-                                    toStringList(list, format)
+                                .map { elements: List<Element> ->
+                                    elements.map(presenter)
                                 }
                         }
                 }
         }.forEach(onSuccess = effect, onFailure = { it.printStackTrace() })
 }
+
+const val format: String = """First Name : %s
+    Last Name : %s
+    Email : %s
+    Salary : %s
+    """
+
+private val elementNames = List("firstName", "lastName", "email", "salary")
+
+private fun processElement(element: Element): String =
+    String.format(format, *elementNames.map { element.getChildText(it) }
+        .toArrayList()
+        .toArray())
 
 fun getXmlFilePath(): FilePath = FilePath("./src/file.xml")
 
@@ -105,7 +97,8 @@ fun getRootElementName(): ElementName = ElementName("staff")
 fun main() {
     val program = readXmlFile(::getXmlFilePath,
                               ::getRootElementName,
-                              format, ::process)
+                              ::processElement,
+                              ::printList)
 
     program()
 }
